77dc21d13444a705175c411f4eb15db14b3fad91
Deepaksuthan <selvalatha.deepak111@gmail.com>
2024-01-04 01:14:58 +0530

    commit

--- src/Components/Header/RTA.js
+++ src/Components/Header/RTA.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RTA = () => {
   return (
     <div className='rta'>
-        <a href='https://drive.google.com/file/d/1C8_zLMFreAfxMyECJIeIZeDzZ7f_G9Ge/view?usp=drive_link' className='btn' target='_blank' rel="noreferrer">Get Resume</a>
+        <a href='https://drive.google.com/file/d/1JgCwisgQuFRUllE2jaZo47oeKsyGe1Xs/view?usp=drive_link' className='btn' target='_blank' rel="noreferrer">Get Resume</a>
         <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
